ae2728a44b77e62cb8b17901b67574d0e558055d
Maruf Monower <marufmonower@gmail.com>
2024-09-26 12:54:39 +0600

    views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Product
+from django.http import HttpResponse
 
-# Create your views here.
+def product_list(request):
+   products = Product.objects.all()
+   return render(request,'index.html',{products:products})
+
+def product_detail(request,pk):
+   product = get_object_or_404(Product, pk=pk)
+   return render(request,'index2.html',{product:'product'})
+
+def home (request):
+   return HttpResponse('hello')
+
